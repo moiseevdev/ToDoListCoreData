e1fc5f0205e560ebc9e5cffeab9d041dd2d9bade
Moiseev Andrei <andreimoiseew2009@yandex.ru>
2021-02-19 23:58:23 +0300

    Initial Commit

---
+++ ToDoListCoreData/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ToDoListCoreData
+//
+//  Created by Андрей Моисеев on 19.02.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
