5c484b96dd17223df99f04d2cb4821069ca255a5
Moiseev Andrei <andreimoiseew2009@yandex.ru>
2021-02-21 23:59:37 +0300

    update TableViewController

--- ToDoListCoreData/TableViewController.swift
+++ ToDoListCoreData/TableViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import CoreData
 
 class TableViewController: UITableViewController {
 
@@ -16,25 +17,51 @@ class TableViewController: UITableViewController {
         let alertController = UIAlertController(title: "New task", message: "Enter a new task", preferredStyle: .alert)
         let saveAction = UIAlertAction(title: "Save", style: .default) { action in
             let textField = alertController.textFields?.first
-            if let newTaskTittle = textField?.text {
-                self.saveTask(withTittle: newTaskTittle)
+            if let newTaskTitle = textField?.text {
+                self.saveTask(withTitle: newTaskTitle)
                 self.tableView.reloadData()
             }
         }
         
         alertController.addTextField { _ in }
-        let cancelAction = UIAlertAction(title: "Cancel", style: .default) { _ in }
+        let cancelAction = UIAlertAction(title: "Cancel", style: .destructive) { _ in }
+        //let testAction = UIAlertAction(title: "test", style: .default) { _ in }
         
         alertController.addAction(saveAction)
         alertController.addAction(cancelAction)
+        //alertController.addAction(testAction)
         
         present(alertController, animated: true, completion: nil)
         
     }
     
-    private func saveTask(withTittle tittle: String) {
+    private func saveTask(withTitle title: String) {
+        let context = getContext()
+        
+        guard let entity = NSEntityDescription.entity(forEntityName: "Task", in: context) else { return }
+        
+        let taskObject = Task(entity: entity, insertInto: context)
+        taskObject.tittle = title
+        
+        do {
+            try context.save()
+        } catch let error as NSError {
+            print(error.localizedDescription)
+        }
         
     }
+    
+    private func getContext() ->NSManagedObjectContext {
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        return appDelegate.persistentContainer.viewContext
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        let context = getContext()
+    }
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
